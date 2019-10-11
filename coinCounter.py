405e176ed624cbc8460a75544db99833e5eadc72
Cerys-Mai <44923095+Cerys-Mai@users.noreply.github.com>
2019-10-11 13:47:00 +0100

    Update coinCounter.py
    add additional comments and change user input method

--- coinCounter.py
+++ coinCounter.py
@@ -1,43 +1,43 @@
 '''This is a program to find the minium number of coins to make a total'''
 
-import sys
-for line in sys.stdin:
+#ask the user to input a number for the total
+line = input("Please select a number for the total: ")
 
-    #variable that converts input to int value
-    total = int(line)
+#variable that converts input to int value
+total = int(line)
 
-    #set up coins with different values
-    coinOne = 5
-    coinTwo = 3
-    coinThree = 1
+#set up coins with different values
+coinOne = 5
+coinTwo = 3
+coinThree = 1
 
-    #variable to count the number of coins used
-    count = 0
+#variable to count the number of coins used
+count = 0
 
-    #variable to count up to total
-    sum = 0
+#variable to count up to total
+sum = 0
 
-    #loop to count ot total
-    while sum<total:
-        sum = sum + coinOne
-        count += 1
-        if sum>total:
-            sum = sum - coinOne
-            count -= 1
+#loop to count ot total
+while sum<total:
+    sum = sum + coinOne
+    count += 1
+    if sum>total:
+        sum = sum - coinOne
+        count -= 1
 
-    while sum<total:
-        sum = sum + coinTwo
-        count += 1
-        if sum>total:
-            sum = sum - coinTwo
-            count -= 1
+while sum<total:
+    sum = sum + coinTwo
+    count += 1
+    if sum>total:
+        sum = sum - coinTwo
+        count -= 1
 
-    while sum<total:
-        sum = sum + coinThree
-        count += 1
-        if sum>total:
-            sum = sum - coinThree
-            count -= 1
+while sum<total:
+    sum = sum + coinThree
+    count += 1
+    if sum>total:
+        sum = sum - coinThree
+        count -= 1
 
-    #print count
-    print(count)
+#print count
+print(count)
